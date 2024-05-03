c7ff626b21c14d9d98e10ebc8c5e675172fc8983
Nathan <nathan.schwarz02@edu.univ-fcomte.fr>
2024-05-03 10:23:12 +0200

    ajout pocketbase

--- src/backend.ts
+++ src/backend.ts
@@ -1,6 +1,6 @@
-import PocketBase from 'pocketbase'
+import pocketbase from 'pocketbase'
 import { type TypedPocketBase } from './pocketbase-types.js'
 
-export const pb = new PocketBase(import.meta.env.VITE_URL_POCKETBASE) as TypedPocketBase
+export const pb = new pocketbase(import.meta.env.VITE_URL_POCKETBASE) as TypedPocketBase
 
 // Copier ici les fonctions developpées en R214 | Système d'information
